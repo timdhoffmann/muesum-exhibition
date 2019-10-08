42b85891199a0f2efe6e6e859b2a891529d56310
jiexdrop <jorge.noogueira@gmail.com>
2019-10-08 11:22:27 +0200

    Changing room to dubugLog

---
+++ Assets/_Project/Scripts/Player.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 10.0F;
+    public bool lockCursor = false;
+    public bool isJumping = false;
+    public bool isGrounded = false;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.name == "Room")
+        {
+            Debug.Log("Entered room");
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.name == "Room")
+        {
+            isGrounded = false;
+        }
+    }
+
+    void Update()
+    {
+        float translation = Input.GetAxis("Vertical") * speed;
+        float straffe = Input.GetAxis("Horizontal") * speed;
+
+        translation *= Time.deltaTime;
+        straffe *= Time.deltaTime;
+
+        transform.Translate(straffe, 0, translation);
+
+        if (Input.GetKey(KeyCode.Space) && isGrounded)
+        {
+            GetComponent<Rigidbody>().AddForce(Vector3.up * 150);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            lockCursor = !lockCursor;
+            if (lockCursor)
+            {
+                Debug.Log("NO_LOCK");
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = true;
+            }
+            else
+            {
+                Debug.Log("LOCK");
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+            }
+        }
+    }
+
+
+}
