22fdfeeb908ecb4426190b9d623cc38b7bcd2ac7
jiexdrop <jorge.noogueira@gmail.com>
2019-10-10 17:47:17 +0200

    Menu enhancements

--- Assets/_Project/Scripts/GameManager.cs
+++ Assets/_Project/Scripts/GameManager.cs
@@ -2,14 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+<<<<<<< Updated upstream
 
 public class GameManager : MonoBehaviour
 {
+=======
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public Image square;
+
+>>>>>>> Stashed changes
     void Update()
     {
 
     }
 
+<<<<<<< Updated upstream
+=======
+    public void ShowSquare()
+    {
+        square.gameObject.SetActive(true);
+    }
+
+    public void HideSquare()
+    {
+        square.gameObject.SetActive(false);
+    }
+
+>>>>>>> Stashed changes
     public void LoadExhibition()
     {
         SceneManager.LoadScene("Exhibition");
